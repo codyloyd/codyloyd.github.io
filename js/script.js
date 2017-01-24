65432bc29e273819c5e320150f6778cc61e20083
Cody Loyd <codyloyd@gmail.com>
2017-01-24 05:58:15 -0600

    sticky nav

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//nav stuff
+const nav = document.querySelector(".nav")
+const topOfNav = nav.offsetTop;
+
+function fixNav(){
+  if (window.scrollY >= topOfNav){
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    nav.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    nav.classList.remove('fixed-nav');
+  }
+}
+window.addEventListener('scroll',fixNav)
