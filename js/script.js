58ab36f1590c33c523d7da7ebf3593df8c2e395f
Cody Loyd <codyloyd@gmail.com>
2017-01-24 13:59:21 -0600

    linkswork

--- js/script.js
+++ js/script.js
@@ -13,6 +13,20 @@ function fixNav(){
 }
 window.addEventListener('scroll',fixNav)
 
+//link scrolling
+navLinks = document.querySelectorAll('.nav > a')
+navLinks.forEach(l => l.addEventListener('click', handleNavClick))
+function handleNavClick(e) {
+  e.preventDefault();
+  const section = document.querySelector(`.${this.dataset.link}`)
+  const top = section.getBoundingClientRect().top + window.scrollY;
+  console.log(top)
+  window.scrollTo(0,top-90);
+  // document.querySelector(`.${this.dataset.link}`).scrollIntoView({ 
+  //   behavior: 'smooth' 
+  // });
+}
+
 //projects thing
 
 const projects = document.querySelectorAll(".project-list > a")
@@ -26,7 +40,6 @@ function highlightProject(project,offset=0){
         top: projectCoords.top + window.scrollY,
         left: projectCoords.left + window.scrollX,
       }
-  console.log(coords)
   slider.style.height = `${coords.height}px`
   slider.style.transform = `translate(${coords.left}px, ${coords.top - 80 - 50 + offset}px)`
   slider.style.display = 'block'
