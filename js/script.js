e675cb3c1b2919a1096c97019117e851a522e385
Cody Loyd <codyloyd@gmail.com>
2017-02-01 21:02:40 -0600

    projects thing looks good

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
-//nav stuff
-const nav = document.querySelector(".nav")
-const topOfNav = nav.offsetTop;
+//nav stuff.. only active if window width is > 800
+if (document.documentElement.clientWidth > 800) {
+  const nav = document.querySelector(".nav")
+  const topOfNav = nav.offsetTop;
 
-function fixNav(){
-  if (window.scrollY >= topOfNav){
-    document.body.style.paddingTop = nav.offsetHeight + 50 + 'px';
-    nav.classList.add('fixed-nav');
-  } else {
-    document.body.style.paddingTop = 0;
-    nav.classList.remove('fixed-nav');
+  function fixNav(){
+    if (window.scrollY >= topOfNav){
+      document.body.style.paddingTop = nav.offsetHeight + 50 + 'px';
+      nav.classList.add('fixed-nav');
+    } else {
+      document.body.style.paddingTop = 0;
+      nav.classList.remove('fixed-nav');
+    }
   }
+  window.addEventListener('scroll',fixNav)
 }
-window.addEventListener('scroll',fixNav)
 
 //link scrolling
 const navLinks = document.querySelectorAll('.nav > a')
@@ -45,12 +47,23 @@ function highlightProject(project,offset=0){
   slider.style.display = 'block'
 }
 
+function loadProject(target){
+  const project = document.querySelector(`#${target}`)
+  const descriptions = document.querySelectorAll(".description-text")
+  descriptions.forEach(d => d.style.display = "none")
+  project.style.display = "block"
+}
+
 
 function handleProjectClick(e) {
   e.preventDefault();
   highlightProject(this)
+  loadProject(this.target)
 }
 
 projects.forEach(p => p.addEventListener('click', handleProjectClick))
 const firstProject = projects[0];
+const firstDescription = document.querySelectorAll(".description-text")[0]
+console.log(firstDescription)
 highlightProject(firstProject,5)
+loadProject(firstDescription.id)
