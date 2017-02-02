a175319d7aa792dc3bb491a94b77ada1eb9bf3ff
Cody Loyd <codyloyd@gmail.com>
2017-02-02 08:15:44 -0600

    mobile functional

--- js/projects.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#tablabels > li').click(function(){
-      var target = $(this).attr("target")
-      $("#tabcontents > li").addClass("hidden");
-      $('#'+target).removeClass("hidden")
-      $("#tablabels > li").removeClass("selectedtab")
-      $(this).addClass('selectedtab')
-  })
-});
